919c4341db6cbcc8f1e06602708c17a0819604ae
bradley.davis <bradley.davis@okg.com>
2023-04-19 15:31:00 -0500

    clean up

--- darwin/default.nix
+++ darwin/default.nix
@@ -4,13 +4,6 @@
 
 let
   system = "aarch64-darwin";
-
-  # pkgs = import nixpkgs {
-  #   inherit system;
-  #   config.allowUnfree = true;
-  # };
-  # lib = nixpkgs.lib;
-
   macbookHostName = "mac";
 in
 {
@@ -28,7 +21,9 @@ in
       home-manager.darwinModules.home-manager {
         home-manager.useGlobalPkgs = true;
         home-manager.useUserPackages = true;
-        home-manager.extraSpecialArgs = { inherit username userDescription; };
+        home-manager.extraSpecialArgs = {
+          inherit username userDescription;
+        };
         home-manager.users.${username} = {
           imports = [
             ./home.nix
