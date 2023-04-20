757b20418e3a30162baea565baf2a92874c8a78a
bradley.davis <bradley.davis@okg.com>
2023-04-19 22:19:00 -0500

    migrate starship config

--- config/default.nix
+++ config/default.nix
@@ -4,6 +4,7 @@
       ".ackrc".source = ./files/ackrc;
       ".config/alacritty/alacritty.yml".source = ./files/alacritty/alacritty.yml;
       ".config/alacritty/themes/solarized_dark.yaml".source = ./files/alacritty/themes/solarized_dark.yaml;
+      ".config/starship.toml".source = ./files/starship.toml;
     };
   };
 }
