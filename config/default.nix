e44ea13d45cde2b7046331d422e04b16ead322df
bradley.davis <bradley.davis@okg.com>
2023-04-19 22:41:08 -0500

    move more stuff

--- config/default.nix
+++ config/default.nix
@@ -4,7 +4,17 @@
       ".ackrc".source = ./files/ackrc;
       ".config/alacritty/alacritty.yml".source = ./files/alacritty/alacritty.yml;
       ".config/alacritty/themes/solarized_dark.yaml".source = ./files/alacritty/themes/solarized_dark.yaml;
+      ".config/nvim/coc-settings.json".source = ./files/nvim/coc-settings.json;
+      ".config/nvim/init.vim".source = ./files/vimrc;
       ".config/starship.toml".source = ./files/starship.toml;
+      ".vimrc".source = ./files/vimrc;
+
+      ".tmux.conf".source = ./files/tmux.conf;
+      ".tmux/linux.conf".source = ./files/tmux/linux.conf;
+      ".tmux/mac.conf".source = ./files/tmux/mac.conf;
+      ".tmux/remote.conf".source = ./files/tmux/remote.conf;
+      ".tmux/snapshot.conf".source = ./files/tmux/snapshot.conf;
+      ".tmux/tpm.conf".source = ./files/tmux/tpm.conf;
     };
   };
 }
