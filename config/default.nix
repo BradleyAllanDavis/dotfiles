d20813a7b2d25a8128eaeca1f656067308989d84
bradley.davis <bradley.davis@okg.com>
2023-04-19 22:16:43 -0500

    migrate alacritty config

--- config/default.nix
+++ config/default.nix
@@ -2,6 +2,8 @@
   home = {
     file = {
       ".ackrc".source = ./files/ackrc;
+      ".config/alacritty/alacritty.yml".source = ./files/alacritty/alacritty.yml;
+      ".config/alacritty/themes/solarized_dark.yaml".source = ./files/alacritty/themes/solarized_dark.yaml;
     };
   };
 }
