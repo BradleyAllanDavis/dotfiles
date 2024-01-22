22b7df2bbbc1b195577f59a7060c9c808817e5d3
bradley.davis <bradley.davis@okg.com>
2024-01-21 21:12:20 -0600

    update server network interfaces

--- machines/server/default.nix
+++ machines/server/default.nix
@@ -47,20 +47,27 @@
           prefixLength = 24;
         }];
       };
-      # enp3s0 = {
-      #   useDHCP = false;
-      #   ipv4.addresses = [{
-      #     address = "192.168.7.102";
-      #     prefixLength = 24;
-      #   }];
-      # };
-      # enp7s0 = {
-      #   useDHCP = false;
-      #   ipv4.addresses = [{
-      #     address = "192.168.7.103";
-      #     prefixLength = 24;
-      #   }];
-      # };
+      enp3s0 = {
+        useDHCP = false;
+        ipv4.addresses = [{
+          address = "192.168.7.102";
+          prefixLength = 24;
+        }];
+      };
+      enp6s0 = {
+        useDHCP = false;
+        ipv4.addresses = [{
+          address = "192.168.7.103";
+          prefixLength = 24;
+        }];
+      };
+      enp7s0 = {
+        useDHCP = false;
+        ipv4.addresses = [{
+          address = "192.168.7.103";
+          prefixLength = 24;
+        }];
+      };
     };
   };
 
