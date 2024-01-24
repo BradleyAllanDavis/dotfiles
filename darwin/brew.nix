d491b61cc7061860290c841cd271d799917d61a0
bradley.davis <bradley.davis@okg.com>
2024-01-23 21:41:44 -0600

    update brews for macbook

--- darwin/brew.nix
+++ darwin/brew.nix
@@ -3,13 +3,13 @@
     enable = true;
     onActivation.autoUpdate = true;
     onActivation.upgrade = false;
-    taps = [
-      "epk/epk"
-      "homebrew/bundle"
-      "homebrew/cask"
-      "homebrew/core"
-      "homebrew/services"
-    ];
+    # taps = [
+    #   "epk/epk"
+    #   "homebrew/bundle"
+    #   "homebrew/cask"
+    #   "homebrew/core"
+    #   "homebrew/services"
+    # ];
     brews = [
       "automake"
       "awscli"
@@ -130,11 +130,11 @@
       "webcatalog"
     ];
     masApps = {
-      "24 Hour Wallpaper" = 1226087575;
-      "Amphetamine" = 937984704;
-      "Bear" = 1091189122;
-      "Boop" = 1518425043;
-      "CodeRunner" = 955297617;
+      # "24 Hour Wallpaper" = 1226087575;
+      # "Amphetamine" = 937984704;
+      # "Bear" = 1091189122;
+      # "Boop" = 1518425043;
+      # "CodeRunner" = 955297617;
       # "Codye" = 1516894961;
       # "com.echodot.Red" = 1491764008;
       "Cookbye" = 1580777168;
@@ -142,38 +142,38 @@
       # "Cryptowatch Desktop" = 1527457231;
       "DaisyDisk" = 411643860;
       "Developer" = 640199958;
-      "Display Maid" = 450063525;
-      "GarageBand" = 682658836;
-      "GIF Keyboard" = 1043270657;
+      # "Display Maid" = 450063525;
+      # "GarageBand" = 682658836;
+      # "GIF Keyboard" = 1043270657;
       "HazeOver" = 430798174;
       # "Hex Fiend" = 1342896380;
       # "HP Smart" = 1474276998;
-      "iMovie" = 408981434;
-      "Keynote" = 409183694;
+      # "iMovie" = 408981434;
+      # "Keynote" = 409183694;
       "Kindle" = 405399194;
       # "LimeChat" = 414030210;
       # "Medis" = 1579200037;
-      "Messenger" = 1480068668;
+      # "Messenger" = 1480068668;
       "Moom" = 419330170;
-      "Noizio" = 928871589;
-      "Numbers" = 409203825;
-      "Pages" = 409201541;
+      # "Noizio" = 928871589;
+      # "Numbers" = 409203825;
+      # "Pages" = 409201541;
       "Paste" = 967805235;
       # "Patterns" = 429449079;
       # "Peek" = 1554235898;
       "PopClip" = 445189367;
-      "Quiver" = 866773894;
+      # "Quiver" = 866773894;
       "RadarScope" = 432027450;
-      "Slack" = 803453959;
+      # "Slack" = 803453959;
       # "SnippetsLab" = 1006087419;
       "Telegram" = 747648890;
       "TestFlight" = 899247664;
       "Things" = 904280696;
-      "ToothFairy" = 1191449274;
-      "Transmit" = 1436522307;
-      "WhatsApp" = 1147396723;
-      "Xcode" = 497799835;
-      "Zones" = 1612523936;
+      # "ToothFairy" = 1191449274;
+      # "Transmit" = 1436522307;
+      # "WhatsApp" = 1147396723;
+      # "Xcode" = 497799835;
+      # "Zones" = 1612523936;
     };
   };
 }
