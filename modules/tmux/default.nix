a153755bfd0ee53cea89b26d17da680047b00e29
bradley.davis <bradley.davis@okg.com>
2024-06-02 21:23:46 -0500

    attempt to fix tmux

--- modules/tmux/default.nix
+++ modules/tmux/default.nix
@@ -3,11 +3,6 @@
   home = {
     file = {
       ".tmux.conf".source = ./tmux.conf;
-      # ".tmux/linux.conf".source = ./linux.conf;
-      # ".tmux/mac.conf".source = ./mac.conf;
-      # ".tmux/remote.conf".source = ./remote.conf;
-      # ".tmux/snapshot.conf".source = ./snapshot.conf;
-      # ".tmux/tpm.conf".source = ./tpm.conf;
     };
   };
   programs.tmux = {
